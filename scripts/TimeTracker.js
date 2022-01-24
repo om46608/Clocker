9e1534c1bb98f5b0710c6c409348fc4e3ed9f64a
Mateusz Ostapko <om46608@zut.edu.pl>
2022-01-24 02:33:51 +0100

    fix zeroing time counter upon restaring a task

--- scripts/TimeTracker.js
+++ scripts/TimeTracker.js
@@ -6,20 +6,51 @@ function startStopTask(id = 0) {
     countTime(id);
 }
 
+function getStartingTime(id) {
+    let timeEl = document.querySelectorAll('#tracker__task__time-' + id)[0];
+    var p = timeEl.value.split(':'),
+        s = 0, m = 1;
+
+    while (p.length > 0) {
+        s += m * parseInt(p.pop(), 10);
+        m *= 60;
+    }
+
+    return s*1000;
+
+}
+
 function countTime(id = 0) {
+    var startingTime = getStartingTime(id);
+
     if (!isActive) {
         isActive = true;
-        startTime = Date.now();
 
-        setInterval(() => {
-            if (isActive) {
-                updateDisplayTime(Date.now() - startTime, id);
-            }
-        }, 1000);
+        if(startingTime === 0) {
+            startTime = Date.now();
+            console.log('jest zero')
+
+            setInterval(() => {
+                if (isActive) {
+                    updateDisplayTime(Date.now() - startTime, id);
+                }
+            }, 1000);
+
+
+        } else {
+            startTime = Date.now();
+
+            setInterval(() => {
+                if (isActive) {
+                    updateDisplayTime(startingTime + Date.now() - startTime, id);
+                }
+            }, 1000);
+
+        }
     } else {
         isActive = false;
         stopTime = Date.now();
-        addToDatabase(Date.now() - startTime, id);
+        addToDatabase(startingTime  + Date.now() - startTime, id);
         stopTime = 0;
         startTime = 0;
     }
@@ -27,6 +58,7 @@ function countTime(id = 0) {
 
 
 function updateDisplayTime(time, id = 0) {
+    console.log(time);
     time = parseTimeDb(time);
     let timeEl = document.querySelectorAll('#tracker__task__time-' + id)[0];
     if (timeEl) {
