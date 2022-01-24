efb608cbc507bb5250bed4bfbe75fed1588073c2
Mateusz Ostapko <om46608@zut.edu.pl>
2022-01-24 03:28:25 +0100

    update db name for deploy

--- config/config.php
+++ config/config.php
@@ -1,6 +1,6 @@
 <?php
 define('URL_ROOT','http://localhost/mvclocker'); //to nie zadziała z obecną nazwą folderu np.
 DEFINE('host', 'localhost');
-DEFINE('dbname', 'clockerdb');
+DEFINE('dbname', 'clockerDB');
 DEFINE('dbuser', 'root');
 DEFINE('dbpass', 'root');
